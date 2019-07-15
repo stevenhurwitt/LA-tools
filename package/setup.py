63399446920472a3a42fc44905308f895b29e257
Steven Hurwitt <stevenhurwitt@gmail.com>
2019-07-14 21:03:29 -0500

    Add files via upload

---
+++ package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='LA-tools',
+       version='0.1',
+       description='tools to help with LA activities',
+       url='https://github.com/stevenhurwitt/LA0tools',
+       author='Steven Hurwitt',
+       author_email='wb5888@external.engie.com',
+       license='GNU GPLv3',
+       packages=['la_tools'],
+       zip_safe=False)
