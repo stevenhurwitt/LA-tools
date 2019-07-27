f01de92631365c58f5b5ea02854fcf074da7002c
Steven Hurwitt <stevenhurwitt@gmail.com>
2019-07-27 15:20:17 -0500

    Update jupyterhub_config.py

--- jupyterhub-master/jupyterhub_config.py
+++ jupyterhub-master/jupyterhub_config.py
@@ -10,7 +10,16 @@ c.JupyterHub.services = [
     },
     {
         'name': 'accounts',
-        'url': '104.210.158.231:10102',
+        'url': '104.210.158.231',
         'command': [sys.executable, './accounts.py'],
     },
 ]
+
+c.JupyterHub.template_paths = ['/opt/tljh/user/share/jupyterhub/templates/']
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import OAuth2WebServerFlow
+
+flow = flow_from_clientsecrets('/opt/tljh/client_secrets.json',
+                               scope='https://www.googleapis.com/auth/',
+                               redirect_uri='http://http://tljh-engie.southcentralus.cloudapp.azure.com/hub/auth_return')
