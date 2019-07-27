d0cf37a488e8780e6e14c6531165ac1f4a4de763
Steven Hurwitt <stevenhurwitt@gmail.com>
2019-07-27 15:45:39 -0500

    Update jupyterhub_config.py

--- jupyterhub-master/jupyterhub_config.py
+++ jupyterhub-master/jupyterhub_config.py
@@ -1,20 +1,5 @@
 import sys
 
-c.Authenticator.admin_users = set(["dblank"])
-
-c.JupyterHub.services = [
-    {
-        'name': 'public',
-        'url': 'http://104.210.158.231',
-        'command': [sys.executable, './public_handler.py'],
-    },
-    {
-        'name': 'accounts',
-        'url': '104.210.158.231',
-        'command': [sys.executable, './accounts.py'],
-    },
-]
-
 c.JupyterHub.template_paths = ['/opt/tljh/user/share/jupyterhub/templates/']
 
 from oauth2client.client import flow_from_clientsecrets
